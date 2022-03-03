34277e96d866d7d7bfb0365e8cb99d0d4033054b
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-03 15:36:38 -0500

    Hover

--- components/ChatMessage.tsx
+++ components/ChatMessage.tsx
@@ -4,12 +4,12 @@ const ChatMessage = ({ message, user }: { message: any; user: any }) => {
   const { text, uid, photoURL } = message
 
   return (
-    <div className="inline-flex bg-slate-50 w-96 p-2 rounded-lg shadow-sm border items-center">
+    <div className="inline-flex bg-slate-50 hover:bg-slate-100 transition-all w-96 p-2 rounded-lg shadow-sm border items-center gap-2">
       <img
         src={photoURL || 'https://picsum.photos/200'}
         className="w-8 rounded-full"
       />
-      <p>{text}</p>
+      <p className='text-slate-700'>{text}</p>
     </div>
   )
 }
