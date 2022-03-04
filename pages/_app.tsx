aa411b5ce0bef9a880e9e3349d2bd079004a03be
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-04 16:26:13 -0500

    setDox

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import { auth, db, provider } from '../firebase'
 import Login from './Login'
 import Loading from '../components/Loading'
 import { useEffect } from 'react'
-import { collection, doc, setDoc } from 'firebase/firestore'
+import { collection, doc, serverTimestamp, setDoc } from 'firebase/firestore'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const [user, loading] = useAuthState(auth)
@@ -14,14 +14,18 @@ function MyApp({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
     if (user) {
-      setDoc(doc(userCollection, user.uid), {
-        displayName: user.displayName,
-        photoURL: user.photoURL,
-        email: user.email,
-
-      })
+      setDoc(
+        doc(userCollection, user.uid),
+        {
+          displayName: user.displayName,
+          photoURL: user.photoURL,
+          email: user.email,
+          lastSeen: serverTimestamp(),
+        },
+        { merge: true }
+      )
     }
-  })
+  }, [user])
 
   if (loading) return <Loading />
   if (!user) return <Login />
