1038e6f6f379cbc6b62bc4b37dea4b5cfbf4b742
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-05 18:13:11 -0500

    Meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,25 +1,33 @@
 import Document, {
-    Html,
-    Head,
-    Main,
-    NextScript,
-    DocumentContext,
-  } from "next/document";
-  
-  export default class MyDoc extends Document {
-    static async getInitialProps(ctx: DocumentContext) {
-      const initialProps = await Document.getInitialProps(ctx);
-      return initialProps;
-    }
-    render(): any {
-      return (
-        <Html>
-          <Head />
-          <body className="box-border bg-white dark:bg-slate-800">
-            <Main />
-            <NextScript />
-          </body>
-        </Html>
-      );
-    }
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from 'next/document'
+
+export default class MyDoc extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx)
+    return initialProps
   }
+  render(): any {
+    return (
+      <Html>
+        <Head />
+        <meta charSet="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <meta
+          name="viewport"
+          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+        />
+        <meta name="description" content="Description" />
+        <meta name="keywords" content="Keywords" />
+        <body className="box-border bg-white dark:bg-slate-800">
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
