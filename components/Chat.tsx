359edfc889047c6d8d34e9999f1b0c3ddd996830
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-04 21:31:45 -0500

    Profile Pic and Workaround if unavailable

--- components/Chat.tsx
+++ components/Chat.tsx
@@ -4,7 +4,6 @@ import { useAuthState } from 'react-firebase-hooks/auth'
 import { useCollection } from 'react-firebase-hooks/firestore'
 import { auth, db } from '../firebase'
 import getRecipientEmail from '../lib/getRecipientEmail'
-import getUserEmail from '../lib/getRecipientEmail'
 
 interface ChatProps {
   id: string
@@ -14,24 +13,35 @@ interface ChatProps {
 const Chat = ({ id, users }: ChatProps) => {
   const [user] = useAuthState(auth)
 
-	const userRef = query(
+  const userRef = query(
     collection(db, 'users'),
     where('email', '==', getRecipientEmail(users, user))
   )
   const [recipientSnapshot] = useCollection(userRef)
 
-  const recipientEmail = getUserEmail(users, user)
+  const recipient = recipientSnapshot?.docs[0]?.data()
+  const recipientEmail = getRecipientEmail(users, user)
+
+  console.log(recipient)
 
   return (
     <div>
       <div>
-        <button className="inline-flex w-full items-center gap-2 break-all rounded-md border border-slate-200 bg-white px-3 py-5 text-slate-600 shadow-sm transition hover:bg-slate-50 focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-100">
-          <img
-            className="w-8 rounded-full shadow-sm"
-            src="https://picsum.photos/200"
-            alt=""
-          />
-          <span className="">{recipientEmail}</span>
+        <button className="inline-flex w-full items-center gap-2 rounded-md border border-slate-200 bg-white px-3 py-5 text-slate-600 shadow-sm transition hover:bg-slate-50 focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-100">
+          {recipient?.photoURL ? (
+            <img
+              className="w-8 rounded-full shadow-sm"
+              src={`${recipient?.photoURL}`}
+              alt=""
+            />
+          ) : (
+            <div className="inline-flex h-8 w-8 items-center justify-center rounded-full border-indigo-200 bg-gradient-to-br from-indigo-500 to-violet-500 text-white shadow-sm">
+              {recipientEmail[0].toUpperCase()}
+            </div>
+          )}
+          <span className="break-text text-left">
+            {recipient?.displayName ? recipient?.displayName : recipientEmail}
+          </span>
         </button>
       </div>
     </div>
