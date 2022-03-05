c5c40412c306960b480b4c9aec5f05296b083790
Ayaan Zaveri <ayaanzaveri08@gmail.com>
2022-03-05 15:08:49 -0500

    Emoji Picker is working!!!

--- components/Messages.tsx
+++ components/Messages.tsx
@@ -168,7 +168,6 @@ const Messages = ({ chat, messages }: any) => {
               onChange={(e) => setInput(e.target.value)}
               placeholder="Type a message..."
             />
-            <span>{chosenEmoji?.emoji}</span>
             <button
               className="inline-flex items-center justify-center rounded-md border border-slate-200 bg-white px-3 py-2 text-slate-600 shadow-sm transition hover:bg-slate-50 focus:border-indigo-500 focus:outline-none focus:ring focus:ring-indigo-200 active:bg-indigo-100"
               type="submit"
